2f56f66baf186c0f0cfbee3b0df4e01a6c2dd9b6
ranjit19-b <ranjit1041997kumar@gmail.com>
2024-10-14 19:21:43 +0530

    added

---
+++ Array/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            Console.WriteLine(cars.Length);
+        }
+    }
+}
